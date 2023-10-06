8eca1b744c665361ad249f396b91af6e9b0a71f3
Esteban Bentancor <ebentancor341@gmail.com>
2023-10-05 21:47:42 -0300

    actualizacion 2

--- js/index.js
+++ js/index.js
@@ -30,50 +30,64 @@ document.addEventListener("DOMContentLoaded", function () {
 
             filteredMovies.forEach(movie => {
                 const listItem = document.createElement("li");
-                listItem.classList.add("list-group-item","bg-dark","text-white","d-column", "off-canvas-top"); // off-canvas-top?
-                listItem.innerHTML = `<h5>${movie.title}</h5>
+                // listItem.classList.add("list-group-item", "bg-dark", "text-white", "d-column", "off-canvas-top");
+                listItem.innerHTML = `<li class="list-group-item bg-dark" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"> <h5>${movie.title}</h5> 
                               <p>${movie.tagline}</p>
-                              <p>Vote Average: ${movie.vote_average} &#9733;</p>`;
-                            //agregar estrellas
+                              <p class="estrella">${generarEstrellas(movie.vote_average)}</p>
+                              </li>`;
                 listItem.addEventListener("click", function () {
                     mostrarPeli(movie);
                 });
                 lista.appendChild(listItem);
-            }); 
+            });
+            
+           
         });
 
     }
-  // e_e n_n U.w.   d(-.O)b   d(#.#)b   d(//.o    o.\\)b     -(7w7)-  (-.-) ('-') \/\/\/\/\/(0w0)\/\/\/\/\/  *\(0_0)/* por genero, lo inclui mas arriba  movie.genres.name.toLowerCase().includes(searchTerm)
-// 
+
+    // e_e n_n U.w.   d(-.O)b   d(#.#)b   d(//.o    o.\\)b     -(7w7)-  (-.-) ('-') \/\/\/\/\/(0w0)\/\/\/\/\/  *\(0_0)/* por genero, lo inclui mas arriba  movie.genres.name.toLowerCase().includes(searchTerm)
    
-  function mostrarPeli(movie) {
-    const cualquiercosa = document.getElementsByTagName("header");
-   cualquiercosa.innerHTML=
-    `
-    <div class="offcanvas offcanvas-start show" class="postion0" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
-    
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasLabel">${movie.title}</h5>
-  </div>
 
-  <div class="offcanvas-body">
-  <p>${movie.tagline}</p>
-  </div>
-
-  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-
-</div>`;
-    }
-    
-    // function mostrarPeli(movie) {
-    //     // Mostrar el offcanvas con los detalles de la película
-    //     const offcanvasLabel = document.getElementById("offcanvasLabel");
-    //     const offcanvasBody = document.querySelector(".offcanvas-body");
-
-    //     offcanvasLabel.textContent = movie.title;
-    //     offcanvasBody.innerHTML = `<p>${movie.tagline}</p>`;
-    //     offcanvas.classList.add("show");
-    // }
+   
 });
 
+function mostrarPeli(movie) {
+    const offcanvasTitle = document.getElementById("offcanvasTopLabel");
+    offcanvasTitle.textContent = movie.title;
+    const offcanvasDescription = document.getElementById("offcanvaDescription");
+    offcanvasDescription.textContent = movie.overview;
+    const offcanvaCategories = document.getElementById("offcanvaCategories");
+    movie.genres.forEach(genero=>{
+        offcanvaCategories.innerHTML += `${genero.name} `;
+    })
+   
 
+    const dropdown = `
+    <div class="dropdown d-flex justify-content-end">
+    <button class="btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+       More
+    </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><p class="dropdown-item">Year: ${movie.release_date}</p></li>
+      <li><p class="dropdown-item">Runtime: ${movie.runtime}</p></li>
+      <li><p class="dropdown-item">Budget: ${movie.budget}</p></li>
+      <li><p class="dropdown-item">Revenue: ${movie.revenue}</p></li>
+      </ul>
+    </div> `;
+    
+    offcanvaCategories.innerHTML += dropdown;
+}
+
+
+function generarEstrellas(calificacion) {
+    var estrellasHTML = "";
+    for (var i = 1; i <= 5; i++) {
+        if (i <= calificacion/2) {
+            estrellasHTML += '<span class="fa fa-star checked" id="estrella"></span>';
+        } else {
+            estrellasHTML += '<span class="fa fa-star" id="estrella"></span>';
+        }
+    }
+    return estrellasHTML;
+}
