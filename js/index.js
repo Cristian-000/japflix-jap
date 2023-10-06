8794f2d83e7a9ec283886cc08846ea3f4711e433
juliamelian <mariajuliamelian@gmail.com>
2023-10-06 01:29:55 -0300

    ajustes varios

--- js/index.js
+++ js/index.js
@@ -30,60 +30,55 @@ document.addEventListener("DOMContentLoaded", function () {
 
             filteredMovies.forEach(movie => {
                 const listItem = document.createElement("li");
-                // listItem.classList.add("list-group-item", "bg-dark", "text-white", "d-column", "off-canvas-top");
-                listItem.innerHTML = `<li class="list-group-item bg-dark" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"> <h5>${movie.title}</h5> 
-                              <p>${movie.tagline}</p>
-                              <p class="estrella">${generarEstrellas(movie.vote_average)}</p>
-                              </li>`;
+                listItem.innerHTML = `
+                    <li class="list-group-item bg-dark text-white d-flex justify-content-between" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                        <div class="col-7">
+                            <h5>${movie.title}</h5> 
+                            <p class="text-secondary"><em>${movie.tagline}</em></p>
+                        </div>
+                            <p class="estrella">${generarEstrellas(movie.vote_average)}</p>
+                            </li>`;
                 listItem.addEventListener("click", function () {
                     mostrarPeli(movie);
                 });
                 lista.appendChild(listItem);
             });
-            
-           
+
+
         });
 
     }
 
     // e_e n_n U.w.   d(-.O)b   d(#.#)b   d(//.o    o.\\)b     -(7w7)-  (-.-) ('-') \/\/\/\/\/(0w0)\/\/\/\/\/  *\(0_0)/* por genero, lo inclui mas arriba  movie.genres.name.toLowerCase().includes(searchTerm)
-   
 
-   
+
+
 });
 
 function mostrarPeli(movie) {
     const offcanvasTitle = document.getElementById("offcanvasTopLabel");
-    offcanvasTitle.textContent = movie.title;
     const offcanvasDescription = document.getElementById("offcanvaDescription");
-    offcanvasDescription.textContent = movie.overview;
     const offcanvaCategories = document.getElementById("offcanvaCategories");
-    movie.genres.forEach(genero=>{
-        offcanvaCategories.innerHTML += `${genero.name} `;
-    })
-   
+    const dropdown = document.getElementById("ul-dropdown-menu");
 
-    const dropdown = `
-    <div class="dropdown d-flex justify-content-end">
-    <button class="btn btn-secondary dropdown-toggle " type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-       More
-    </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-      <li><p class="dropdown-item">Year: ${movie.release_date}</p></li>
-      <li><p class="dropdown-item">Runtime: ${movie.runtime}</p></li>
-      <li><p class="dropdown-item">Budget: ${movie.budget}</p></li>
-      <li><p class="dropdown-item">Revenue: ${movie.revenue}</p></li>
-      </ul>
-    </div> `;
-    
-    offcanvaCategories.innerHTML += dropdown;
+    offcanvasTitle.textContent = movie.title;
+    offcanvasDescription.textContent = ` ${movie.overview}`;
+    offcanvaCategories.textContent = movie.genres.map((genre) => ` ` + genre.name);
+
+    dropdown.innerHTML = '';
+    dropdown.innerHTML += `
+      <li class="dropdown-item d-flex justify-content-between"><p>Year: </p><p> ${movie.release_date.substring(0, 4)}</p></li>
+      <li class="dropdown-item d-flex justify-content-between"><p>Runtime: </p><p> ${movie.runtime} mins</p></li>
+      <li class="dropdown-item d-flex justify-content-between"><p>Budget: </p><p> $${movie.budget}</p></li>
+      <li class="dropdown-item d-flex justify-content-between"><p>Revenue:&nbsp;&nbsp; </p><p> $${movie.revenue}</p></li>`;
+    ;
 }
 
 
 function generarEstrellas(calificacion) {
     var estrellasHTML = "";
     for (var i = 1; i <= 5; i++) {
-        if (i <= calificacion/2) {
+        if (i <= calificacion / 2) {
             estrellasHTML += '<span class="fa fa-star checked" id="estrella"></span>';
         } else {
             estrellasHTML += '<span class="fa fa-star" id="estrella"></span>';
